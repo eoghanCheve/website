af4446017832102c3a2f3bc55d9bb9d590d53226
Eoghan Cheve <41449253+eoghanCheve@users.noreply.github.com>
2025-01-13 20:03:52 +0100

    erased debug message in redirect

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,7 +1,6 @@
 const lang = navigator.language;
 const path = window.location.pathname;
 const page = path.split("/").pop();
-alert(page);
 if (lang.startsWith("fr")) {
     window.location.replace("fr/" + page);
 } else {
