480d8307910a89a0a2ba73fcf0b3374724c55496
Eoghan Cheve <41449253+eoghanCheve@users.noreply.github.com>
2025-01-13 19:40:50 +0100

    added redirect for croco

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,6 +1,9 @@
 const lang = navigator.language;
+const path = window.location.pathname;
+const page = path.split("/").pop();
+alert(page);
 if (lang.startsWith("fr")) {
-    window.location.replace("fr/index.html");
+    window.location.replace("fr/" + page);
 } else {
-    window.location.replace("en/index.html");
+    window.location.replace("en/" + page);
 }
