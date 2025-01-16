37350f9266fec6a9bfb70e65c76062156f95c82f
Eoghan Cheve <41449253+eoghanCheve@users.noreply.github.com>
2025-01-16 09:27:00 +0100

    added link to stageL3 with cool js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
-var a;
-//TODO
-//crocodynamique
+function init() {
+    const path = document.URL;
+    const page = path.split("/").pop();
+    var stage_details = document.getElementById("stage-L3-details");
+    if (page.match("career.html#stage-L3")) {
+        stage_details.open = true;
+    }
+}
+
+window.onload = init;
